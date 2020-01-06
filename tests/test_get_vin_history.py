4ca2bf7d4fe3576a69d22057ee817ea4874c52db
muhammad-h264ussain <55404987+muhammad-h264ussain@users.noreply.github.com>
2020-01-06 19:48:58 +0500

    Unit Test add that will test pagination function

--- tests/test_get_vin_history.py
+++ tests/test_get_vin_history.py
@@ -4,7 +4,7 @@ from unittest import TestCase, mock
 
 from market_api.market_api import MarketAPI
 from market_api.schema import GetVinHistoryOutputSchema
-
+import time
 
 class MarketAppTestCase(TestCase):
 
@@ -81,3 +81,22 @@ class MarketAppTestCase(TestCase):
         vin = '2FMGK5D81EBD1433'
         response = client.get_vin_history(vin)
         self.assertEqual(response, 'Vin is not valid: {}'.format(vin))
+        
+        
+    def test_get_vin_history_with_pagination(self):
+        
+        token = os.getenv('API_TOKEN')
+        client = MarketAPI(token)
+
+        vins = {"1FTEW1EF1FFA67753":6,}
+
+        for vin,page_limit in vins.items():
+            for count in range(1,page_limit+1):
+                time.sleep(3)
+                api_response = client.get_vin_history(vin=vin, page=count)
+                last_seen_at_ary = []
+                
+                assert len(api_response) != 0
+            
+                if count != page_limit:
+                    assert len(api_response) == 50
